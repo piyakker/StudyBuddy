d43980c92bdda5727d6d547f8fcb56c7889d933d
piyakker <ericlien86@gmail.com>
2023-02-09 00:25:19 +0800

    add templates folder

--- base/views.py
+++ base/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'home.html')
 
 def rooms(request):
-    return HttpResponse('Rooms page')
+    return render(request, 'room.html')
