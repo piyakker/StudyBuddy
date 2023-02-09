c402bfa1feded0674a2b0759b639773e982b357a
piyakker <ericlien86@gmail.com>
2023-02-09 14:08:08 +0800

    add search bar to home

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .forms import RoomForm
 from .models import Room, Topic
 
@@ -7,7 +8,11 @@ from .models import Room, Topic
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
     topics = Topic.objects.all()
     context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html',  context)
