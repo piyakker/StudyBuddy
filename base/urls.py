ae13ff379574899f1bf749968a5ad5a284c75b5a
piyakker <ericlien86@gmail.com>
2023-02-09 11:57:20 +0800

    send test data to templates

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('rooms/', views.rooms, name='rooms')
+    path('rooms/<str:pk>', views.room, name='room')
 ]
--- base/views.py
+++ base/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
-def home(request):
-    return render(request, 'home.html')
 
-def rooms(request):
-    return render(request, 'room.html')
+# Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Python'},
+    {'id': 2, 'name': 'Django'},
+    {'id': 3, 'name': 'Web Dev'},
+]
+
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html',  context)
+
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+          room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
