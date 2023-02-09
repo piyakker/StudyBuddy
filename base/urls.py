101c4dd8aaf2190c70fc302d0b49659c612b199e
piyakker <ericlien86@gmail.com>
2023-02-09 12:57:33 +0800

    add create-room function

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('rooms/<str:pk>', views.room, name='room')
+    path('rooms/<str:pk>', views.room, name='room'),
+    path('create-room', views.createRoom, name='create-room')
 ]
